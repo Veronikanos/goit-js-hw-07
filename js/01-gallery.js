2457c32eebd01bafb08b9441ba053edd92c415bc
Veronica <vveronikanika@gmail.com>
2022-11-05 14:20:05 +0200

    init: first task

---
+++ js/01-gallery.js
@@ -0,0 +1,42 @@
+import { galleryItems } from './gallery-items.js';
+
+// document.createElement()
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map((item) => {
+	return `
+	<div class="gallery__item">
+		<a class="gallery__link" href="${item.original}">
+		<img 
+			class="gallery__image" 
+			src="${item.preview}"
+			data-source="${item.original}"
+			alt="${item.description}"/>
+		</a>
+	</div>
+`;
+});
+
+galleryList.innerHTML = galleryMarkup.join('');
+
+galleryList.addEventListener('click', onPictureClick);
+
+function onPictureClick(e) {
+	e.preventDefault();
+	if (e.target.nodeName !== 'IMG') {
+		return;
+	}
+
+	const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">`);
+	instance.show();
+
+	galleryList.addEventListener('keydown', onEscBtnPress);
+
+	function onEscBtnPress(e) {
+		if (e.code === 'Escape') {
+			instance.close();
+		}
+	}
+}
